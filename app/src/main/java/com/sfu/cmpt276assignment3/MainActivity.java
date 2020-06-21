7a436b0189a936dcdfd2fd5f15e053b3ca9e3f94
Branko Bajcetic <bajcetic.branko@gmail.com>
2020-06-20 22:21:44 -0700

    Make MainActivity full screen and landscape

--- app/src/main/java/com/sfu/cmpt276assignment3/MainActivity.java
+++ app/src/main/java/com/sfu/cmpt276assignment3/MainActivity.java
@@ -2,10 +2,13 @@ package com.sfu.cmpt276assignment3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
