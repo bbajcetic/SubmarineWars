26c7252de240ff62c0745818d89e174df981287f
Branko Bajcetic <bajcetic.branko@gmail.com>
2020-06-21 11:31:28 -0700

    In the Help screen, make the webpage a link that redirects you to that webpage in a browser when you click on it

--- app/src/main/java/com/sfu/cmpt276assignment3/HelpActivity.java
+++ app/src/main/java/com/sfu/cmpt276assignment3/HelpActivity.java
@@ -1,10 +1,20 @@
 package com.sfu.cmpt276assignment3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -12,6 +22,29 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        setupWebpageLink();
+    }
+
+    private void setupWebpageLink() {
+        TextView aboutContent = findViewById(R.id.text_help_about_content);
+        String text = this.getResources().getString(R.string.help_about_content);
+        int startIndex = text.indexOf("Webpage: ") + "Webpage: ".length();
+        int endIndex = text.length();
+        final String webpage = text.substring(startIndex, endIndex);
+
+        SpannableString ss = new SpannableString(text);
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                Toast.makeText(HelpActivity.this, "ok", Toast.LENGTH_SHORT).show();
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(webpage));
+                startActivity(browserIntent);
+            }
+        };
+        ss.setSpan(clickableSpan, startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        aboutContent.setText(ss);
+        aboutContent.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     public static Intent makeIntent(Context context) {
